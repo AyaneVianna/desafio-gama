8ecc1b36013ae852465832d605fa79dc76820f96
AyaneVianna <ayanemachado09@gmail.com>
2021-08-29 10:27:58 -0300

    Adicionando arquivos do Back-end e algumas modificações no html e css

--- script.js
+++ script.js
@@ -64,4 +64,83 @@ function validacao() {
      }
 }
 
+// ligação front e back
+const Formulario = () => {
+    
+   let form = {
+        nome: document.getElementById('nome').value,
+        cargo: document.getElementById('cargo').value,
+        data: document.getElementById('data').value,
+        genero: document.getElementById('genero').value,
+        estadoCivil: document.getElementById('estadoCivil').value,
+        identidade: document.getElementById('identidade').value,
+        cpf_digitado: document.getElementById('cpf_digitado').value,
+        habilitacao: document.getElementById('habilitacao'),
+        veiculo: document.getElementById('veiculo').value,
+        cep: document.getElementById('cep').value,
+        rua: document.getElementById('rua').value,
+        numero: document.getElementById('numero').value,
+        complemento: document.getElementById('complemento').value,
+        bairro: document.getElementById('bairro').value,
+        cidade: document.getElementById('cidade').value,
+        uf: document.getElementById('uf').value,
+        email: document.getElementById('email').value,
+        telefone: document.getElementById('telefone').value,
+        telefone2: document.getElementById('telefone2').value,
+        celular: document.getElementById('celular').value,
+        celular2: document.getElementById('celular2').value,
+    };
+    console.log(form);
+    return form
+}
+
+const criarCandidato = async (candidato) => {
+
+    const usuario = await fetch('http://localhost:5000/register', {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(Formulario())
+    });
+    if (usuario.status === 200) {
+        alert('Sucesso no cadastro');
+    }
+
+    if (usuario.status === 500) {
+        alert('Ops! Esse CPF ou Email já está cadastrado');
+    }
+}
+
+function check_form() {
+    let nome = document.getElementById('nome').value;
+    let cargo = document.getElementById('cargo').value;
+    let data = document.getElementById('data').value;
+    let identidade = document.getElementById('identidade').value;
+    let cpf_digitado = document.getElementById('cpf_digitado').value;
+    let cep = document.getElementById('cep').value;
+    let rua = document.getElementById('rua').value;
+    let numero = document.getElementById('numero').value;
+    let bairro = document.getElementById('bairro').value;
+    let cidade = document.getElementById('cidade').value;
+    let uf = document.getElementById('uf').value;
+    let email = document.getElementById('email').value;
+    let telefone = document.getElementById('telefone').value;
+    let celular = document.getElementById('celular').value;
+
+    if (nome == "" || cargo == "" || data =="" || identidade == "" ||
+    cpf_digitado == "" || cep == "" || rua =="" || numero == "" ||
+    bairro == "" || cidade == "" || uf == "" || email == "" || telefone == ""
+    || celular == "" || validacao() == false) {
+        alert('Algo deu errado, verifique se os campos estão preenchidos corretamente.');
+    } else {
+        criarCandidato();
+        alert('efetuando o cadastro...');
+    }
+}
+
+
+
+
 
